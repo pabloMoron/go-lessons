1fcc9e94e7b28320bf7b92ea3482eab215d7db68
Pablo Moron <moronpablo94@gmail.com>
2023-10-22 20:48:57 -0300

    ✨ add hello world

---
+++ 01-hello_world/go.mod
@@ -0,0 +1,3 @@
+module 01-hello_world
+
+go 1.21.3
---
+++ 01-hello_world/main.go
@@ -0,0 +1,16 @@
+// All files starts with package
+package main
+
+import (
+	/*
+		Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+		The format 'verbs' are derived from C's but are simpler.
+	*/
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world!") // Add newline
+}
+
+// in console type go run ./main.go
