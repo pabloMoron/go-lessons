2c2df094e30a601ae793f1776ed1d906db7e0b7c
pabloMoron <39250066+pabloMoron@users.noreply.github.com>
2024-04-19 22:06:53 -0300

    ✨ add import

--- 01-hello_world/main.go
+++ 01-hello_world/main.go
@@ -5,12 +5,16 @@ import (
 	/*
 		Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
 		The format 'verbs' are derived from C's but are simpler.
+
+		By convention, the package name is the same as the last element of the import path.
 	*/
 	"fmt"
+	"math/rand"
 )
 
 func main() { // If cloud environment => main() receives params
 	fmt.Println("Hello world!") // Add newline
+	fmt.Println(rand.Intn(10))  // Add newline
 }
 
 // in console type go run ./main.go
