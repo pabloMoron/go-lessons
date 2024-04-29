7fdb9862ba30e0185e4395f6b98b228e5ab0c20c
pabloMoron <39250066+pabloMoron@users.noreply.github.com>
2024-04-29 20:13:27 -0300

    ♻️ rename

--- 02-imports/constants/constants.go
+++ 02-imports/constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 import "fmt"
 
-func ShowInts() {
+func PrintlnInts() {
 	var integer int              // Declaration, variables always are initialized with default value, not null
 	integer_32_bits := int32(10) // Declaration + assignation operator (:=)
 	integer_64_bits := int64(10)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-lessons
 
 go 1.21.3
+
+require github.com/pabloMoron/go-lessons/02-imports v0.0.0-20240420013055-bb066dee4381
