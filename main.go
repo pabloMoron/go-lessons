72a52e4ec03cc92e99aa58d5188027faf8befdeb
pabloMoron <39250066+pabloMoron@users.noreply.github.com>
2024-04-29 20:55:45 -0300

    ♻️

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 		By convention, the package name is the same as the last element of the import path.
 	*/
 	"fmt"
+	"go-lessons/01-hello_world/hello_world"
+	"go-lessons/02-imports/constants"
 	"math/rand"
 )
 
 func main() { // If cloud environment => main() receives params
 	fmt.Println("Random:", rand.Intn(10)) // Add newline
+	constants.PrintlnInts()
+	hello_world.HelloWorld()
 }
 
 // in console type go run ./main.go
